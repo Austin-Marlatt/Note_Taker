33c302403fabb6663bde5d28e41a21b64f64a624
Austin Marlatt <austin.marlatt1@gmail.com>
2024-06-06 22:07:13 -0700

    added code for the server.js file / routes files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+
+});
+
+router.post('/notes', (req, res) => {
+
+});
+
+router.delete('/notes/:id', (req, res) => {
+
+});
